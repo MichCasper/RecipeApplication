f4694b4ed0d9dd0312ae1b29506225d7da40075c
Michael Kasper <kasper_michael@gmx.net>
2024-11-22 09:13:52 +0100

    Add comments to IngredientRepository

--- src/main/java/com/mk/RecipeApplication/ingredient/IngredientRepository.java
+++ src/main/java/com/mk/RecipeApplication/ingredient/IngredientRepository.java
@@ -4,6 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Repository interface for managing Ingredient entities.
+ * Extends JpaRepository to provide basic CRUD operations.
+ */
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    /**
+     * Retrieves all ingredients associated with the given recipe ID.
+     *
+     * @param recipeId the ID of the recipe whose ingredients are to be fetched
+     * @return a list of ingredients belonging to the specified recipe
+     */
     List<Ingredient> findAllByRecipeId(Integer recipeId);
 }
